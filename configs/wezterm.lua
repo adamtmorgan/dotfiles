6eb824c4731e32eb36607bfe193fdeb1f2a5a760
Adam Morgan <38927602+adamtmorgan@users.noreply.github.com>
2024-08-25 22:45:00 -0400

    changed theme

--- configs/wezterm.lua
+++ configs/wezterm.lua
@@ -11,8 +11,8 @@ config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", italic 
 -- Tab bar styling
 config.use_fancy_tab_bar = true
 config.window_frame = {
-	active_titlebar_bg = "#1C1E25",
-	inactive_titlebar_bg = "#1C1E25",
+	active_titlebar_bg = "#1B1825",
+	inactive_titlebar_bg = "#1B1825",
 	font_size = 12.0,
 }
 
@@ -21,7 +21,7 @@ config.colors = {
 	cursor_bg = "#FF9E3B",
 	cursor_border = "#FF9E3B",
 	cursor_fg = "000000",
-	background = "#1B1E25",
+	background = "#1B1825",
 	tab_bar = {
 		inactive_tab_edge = "None",
 		active_tab = {
@@ -29,14 +29,14 @@ config.colors = {
 			fg_color = "#ffffff",
 		},
 		inactive_tab = {
-			bg_color = "#1c2129",
+			bg_color = "#1B1825",
 			fg_color = "#DCD7BA",
 		},
 	},
 }
 
 -- Window settings
-config.window_background_opacity = 0.90
+config.window_background_opacity = 0.94
 config.macos_window_background_blur = 80
 config.window_decorations = "RESIZE"
 
